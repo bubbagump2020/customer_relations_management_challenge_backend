ee7360935bcb0ac0dc376769af55f6274e8f0ce7
Kevin Bagnall <bubbagump2020@gmail.com>
2022-03-24 15:36:13 -0500

    Update clients_controller.rb
    Added in the delete and update methods

--- app/controllers/clients_controller.rb
+++ app/controllers/clients_controller.rb
@@ -1,5 +1,7 @@
 class ClientsController < ApplicationController
 
+  # errors.full_messages is for troubleshooting and will not be included in production
+
   def index
     clients = Client.all
     render json: clients
@@ -15,17 +17,25 @@ class ClientsController < ApplicationController
   end
 
   def update
-
+    client = Client.find_by_id(params[:id])
+    if client.update(client_params)
+      render json: { message: 'Client updated', client: client }
+    else
+      render json: { message: 'Client not updated', errors: client.errors.full_messages }
+    end
   end
 
   def delete
-
+    client = Client.find_by_id(params[:id])
+    if !client
+      render json: { message: 'Client not found', errors: client.errors.full_messages }
+    else
+      render json: { message: 'Client deleted' }
+    end
   end
 
   def client_params
     params.permit(:first_name, :last_name, :email, :stage, :phone, :company, :probability)
   end
-
-
-
+  
 end
